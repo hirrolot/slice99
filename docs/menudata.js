7b4d52bb8c6e4802cf5a4b00b4eba62f816efb2f
hirrolot <hirrolot@gmail.com>
2020-12-28 15:11:52 +0600

    Rework the main page of the docs

--- docs/menudata.js
+++ docs/menudata.js
@@ -9,6 +9,7 @@ var menudata={children:[
 {text:"Files",url:"files.html",children:[
 {text:"File List",url:"files.html"},
 {text:"Globals",url:"globals.html",children:[
-{text:"All",url:"globals.html"},
+{text:"All",url:"globals.html",children:[
+{text:"s",url:"globals.html#index_s"}]},
 {text:"Functions",url:"globals_func.html"},
 {text:"Macros",url:"globals_defs.html"}]}]}]}
--- docs/search/defines_0.js
+++ docs/search/defines_0.js
@@ -1,5 +1,16 @@
 var searchData=
 [
   ['slice99_5farray_5flen',['Slice99_array_len',['../slice99_8h.html#a2c76760ffa8f1b96400c17539684c6da',1,'slice99.h']]],
-  ['slice99_5ffrom_5farray',['Slice99_from_array',['../slice99_8h.html#aee899fa7bd6ee37b88c0bf229213d97e',1,'slice99.h']]]
+  ['slice99_5fassert',['SLICE99_ASSERT',['../slice99_8h.html#a29d489b28cf4d2cf56a17a2617990fee',1,'slice99.h']]],
+  ['slice99_5fbsearch',['SLICE99_BSEARCH',['../slice99_8h.html#aedee03b3135514dfcbdf244d01fa4d35',1,'slice99.h']]],
+  ['slice99_5ffrom_5farray',['Slice99_from_array',['../slice99_8h.html#aee899fa7bd6ee37b88c0bf229213d97e',1,'slice99.h']]],
+  ['slice99_5fget',['Slice99_get',['../slice99_8h.html#a907b544bdd92e9d7a7b3b50c69102fd6',1,'slice99.h']]],
+  ['slice99_5fget_5fcast_5ftype',['Slice99_get_cast_type',['../slice99_8h.html#a80df197030d2e328a1856a68204b7c0b',1,'slice99.h']]],
+  ['slice99_5fmemcmp',['SLICE99_MEMCMP',['../slice99_8h.html#a4ad6e85e51121b718c704bc6f7c16cd7',1,'slice99.h']]],
+  ['slice99_5fmemcpy',['SLICE99_MEMCPY',['../slice99_8h.html#a7306a32a97b3b8a5f05c7b052618930c',1,'slice99.h']]],
+  ['slice99_5fqsort',['SLICE99_QSORT',['../slice99_8h.html#af75364d1b179202146fcf2953356d01a',1,'slice99.h']]],
+  ['slice99_5fstrlen',['SLICE99_STRLEN',['../slice99_8h.html#a74ab39995ce273807783bb0006d9d85d',1,'slice99.h']]],
+  ['slice99_5fsub',['Slice99_sub',['../slice99_8h.html#a2c941abce41d90462e745ef6729e8965',1,'slice99.h']]],
+  ['slice99_5fsub_5fcast_5ftype',['Slice99_sub_cast_type',['../slice99_8h.html#aff5368fbf40b74b98e7f24de59596e8c',1,'slice99.h']]],
+  ['slice99_5fswap',['Slice99_swap',['../slice99_8h.html#ab329a6e614bc483f1306a39a66344343',1,'slice99.h']]]
 ];
