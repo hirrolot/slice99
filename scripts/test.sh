370262bceb26b76bc66690b36855dc7906a573d2
hirrolot <hirrolot@gmail.com>
2020-12-28 15:33:55 +0600

    Execute static code analysis (scripts/test.sh)

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,3 +3,13 @@
 clang tests.c -I. -Weverything -std=c99 -o tests
 ./tests
 rm tests
+
+mkdir -p examples/build
+cd examples/build
+cmake ..
+cmake --build .
+./demo
+cd ../..
+
+cppcheck tests.c
+scan-build --use-analyzer=/usr/bin/clang tests.c
