0f09d9b274d0d8960d61aea15a69b162dcbfb80f
hirrolot <hirrolot@gmail.com>
2020-12-28 15:42:27 +0600

    Fix scripts/test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-clang tests.c -I. -Weverything -std=c99 -o tests
+# Check the tests.
+cppcheck tests.c
+scan-build --use-analyzer=/usr/bin/clang clang tests.c -I. -Weverything -std=c99 -o tests
 ./tests
 rm tests
 
+# Check the examples.
 mkdir -p examples/build
 cd examples/build
 cmake ..
 cmake --build .
 ./demo
 cd ../..
-
-cppcheck tests.c
-scan-build --use-analyzer=/usr/bin/clang tests.c
--- tests.c
+++ tests.c
@@ -28,6 +28,7 @@
 #include <limits.h>
 #include <stdbool.h>
 #include <stdint.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
