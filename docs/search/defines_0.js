7d0ad8a89eb8110af1d2b14275567295231a3941
hirrolot <hirrolot@gmail.com>
2021-05-24 16:01:15 +0600

    Re-generate `docs/`

--- docs/search/defines_0.js
+++ docs/search/defines_0.js
@@ -2,6 +2,7 @@ var searchData=
 [
   ['slice99_5farray_5flen',['SLICE99_ARRAY_LEN',['../slice99_8h.html#a36fc375eb1db0b3907825ea0720fbf0c',1,'slice99.h']]],
   ['slice99_5fassert',['SLICE99_ASSERT',['../slice99_8h.html#a29d489b28cf4d2cf56a17a2617990fee',1,'slice99.h']]],
+  ['slice99_5fdef_5ftyped',['SLICE99_DEF_TYPED',['../slice99_8h.html#a557c011352fbc859d6c9ed3871a31c5e',1,'slice99.h']]],
   ['slice99_5ffrom_5farray',['Slice99_from_array',['../slice99_8h.html#aee899fa7bd6ee37b88c0bf229213d97e',1,'slice99.h']]],
   ['slice99_5ffrom_5ftyped_5fptr',['Slice99_from_typed_ptr',['../slice99_8h.html#af8c2fd6a232198b3c511c19f3546b41b',1,'slice99.h']]],
   ['slice99_5fmajor',['SLICE99_MAJOR',['../slice99_8h.html#aa20d9d653b7c065db03d068ada57b89c',1,'slice99.h']]],
@@ -10,5 +11,8 @@ var searchData=
   ['slice99_5fmemmove',['SLICE99_MEMMOVE',['../slice99_8h.html#afa5c9f55ad339be88c60744fadb239f6',1,'slice99.h']]],
   ['slice99_5fminor',['SLICE99_MINOR',['../slice99_8h.html#a8a577ef72a4eace6cfb5ca89f2518250',1,'slice99.h']]],
   ['slice99_5fpatch',['SLICE99_PATCH',['../slice99_8h.html#a910ddb4ba43cb60535cf6d1c3cb498a0',1,'slice99.h']]],
-  ['slice99_5fstrlen',['SLICE99_STRLEN',['../slice99_8h.html#a74ab39995ce273807783bb0006d9d85d',1,'slice99.h']]]
+  ['slice99_5fstrlen',['SLICE99_STRLEN',['../slice99_8h.html#a74ab39995ce273807783bb0006d9d85d',1,'slice99.h']]],
+  ['slice99_5fto_5ftyped',['SLICE99_TO_TYPED',['../slice99_8h.html#a1268f7fbfc4e44efd9322b4123664e56',1,'slice99.h']]],
+  ['slice99_5fto_5funtyped',['SLICE99_TO_UNTYPED',['../slice99_8h.html#ace6a9c752806e590ca10c3f2131c8665',1,'slice99.h']]],
+  ['slice99_5ftyped_5ffrom_5farray',['Slice99_typed_from_array',['../slice99_8h.html#a910435545a29467d608f27e328cc429d',1,'slice99.h']]]
 ];
