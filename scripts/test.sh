6413ca1d3317d37c5736c2655d1c36a4d5e1211c
hirrolot <hirrolot@gmail.com>
2021-08-20 23:37:10 +0600

    Specify ASan for Clang too

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,11 +3,11 @@
 cppcheck tests.c
 
 # Test Clang with scan-build.
-scan-build --use-analyzer=/usr/bin/clang clang tests.c -I. -Iassert-algebraic -Weverything -std=c99 -o tests
+scan-build --use-analyzer=/usr/bin/clang clang tests.c -I. -Iassert-algebraic -Weverything -std=c99 -o tests -fsanitize=address
 ./tests
 rm tests
 
-# Test GCC with ASan.
+# Test GCC.
 gcc tests.c -I. -Iassert-algebraic -Wall -Wextra -pedantic -std=c99 -o tests -fsanitize=address
 ./tests
 rm tests
